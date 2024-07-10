8affb11c55fa4d9afb2f338bf66e594cf8e27af9
Ramiro Andino <ramiroandino987@gmail.com>
2024-07-10 18:15:40 -0500

    landing created, making it responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
-const colors = require('tailwindcss/colors')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        ...colors
+      colors: {
+        primary: {"main":"#004AAD","verdeclaro":"#b0e5d0","verdeoscuro":"rgba(92,202,238,0.41)"}
       }
+
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.gradient-text': {
+          background: 'radial-gradient( circle farthest-corner at 10% 10%,  rgba(176,229,208,1) 42%, rgba(92,202,238,0.41) 93.6% )',
+          '-webkit-background-clip': 'text',
+          '-webkit-text-fill-color': 'transparent',
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }
+  ],
 }
 
