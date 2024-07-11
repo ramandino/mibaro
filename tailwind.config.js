78ee353159192feb935c27a19e0ae82fc9b8d670
Ramiro Andino <ramiroandino987@gmail.com>
2024-07-11 10:36:51 -0500

    contat page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'selector',
-  content: ["./dist/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
