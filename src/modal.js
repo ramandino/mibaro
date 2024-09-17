21ddb6c1e1d89199bfd989702571c8207ff0cd9f
Ramiro Andino <ramiroandino987@gmail.com>
2024-09-17 16:24:41 -0500

    js innerhtml demo works

--- src/modal.js
+++ src/modal.js
@@ -1,4 +1,4 @@
-import supabase from "./supabaseconfig.js" 
+// import supabase from "./supabaseconfig.js" 
 
 export function openModal(){
     document.addEventListener("DOMContentLoaded",function(){
@@ -32,34 +32,61 @@ export function crear(){
 
 
     crearespacio.addEventListener("submit", (event) => {
+        const modal = document.getElementById("modal")
+        const body = document.getElementById("bodywindow")
+        body.classList.remove("blur-sm")
+        modal.classList.add("hidden")
+
         event.preventDefault()
         const crearnuevo = new FormData(event.target)
         const nuevoObjeto = Object.fromEntries(crearnuevo)
-        test(nuevoObjeto.nombreespacio,nuevoObjeto.nombreplataforma,nuevoObjeto.cantidadinicial,nuevoObjeto.notas)    
+        const nuevoHtml = `
+        <div class="container-caja">
+            <div class="ml-6 mt-4 text-primary-verdeclaro">${nuevoObjeto.nombreespacio}</div>
+            <div class="ml-6 text-white">baro en ${nuevoObjeto.nombreplataforma}:</div>
+            <div class="ml-6 text-2xl mb-4 text-white"> $ ${nuevoObjeto.cantidadinicial}</div>
+        </div>
+    `;
+    areaparacajas.innerHTML += nuevoHtml;
+    crearespacio.reset()
+
+
     } );
 
-    class CrearNuevoEspacio{
-        constructor(nombre, plataforma, cantidad, notas){
-            this.nombreEspacio = nombre
-            this.nombrePlataforma = plataforma
-            this.cantidadInicial = cantidad
-            this.notasAdicionales = notas
-        }    
-    }
 
-    async function test (nombre,plataforma,cantidad,notas){
-        const nuevoespacio = new CrearNuevoEspacio(nombre,plataforma,cantidad,notas)
-        const { error } = await supabase
-            .from('espacios')
-            .insert({ id: 1, space_nombre: nuevoespacio.nombreEspacio})
-        if (error) {
-            console.error('Error inserting data:', error);
-        } else {
-            console.log('Data inserted successfully');
-        }
-    }
+    // class CrearNuevoEspacio{
+    //     constructor(nombre, plataforma, cantidad, notas){
+    //         this.nombreEspacio = nombre
+    //         this.nombrePlataforma = plataforma
+    //         this.cantidadInicial = cantidad
+    //         this.notasAdicionales = notas
+    //     }    
+    // }
+
+//     const nuevoHtml = `
+//     <div class="container-caja">
+//         <div class="ml-6 mt-4 text-primary-verdeclaro">${nombre}]$</div>
+//         <div class="ml-6 text-white">baro en ${plataforma}$:</div>
+//         <div class="ml-6 text-2xl mb-4 text-white"> $ ${cantidad}$</div>
+//     </div>
+// `;
+
+// // Agrega el nuevo HTML al contenedor
+//     areaparacajas.innerHTML = nuevoHtml;
 
     
+    // async function test (nombre,plataforma,cantidad,notas){
+    //     const nuevoespacio = new CrearNuevoEspacio(nombre,plataforma,cantidad,notas)
+    
+    //     const { error } = await supabase
+    //         .from('espacios')
+    //         .insert({ id: 1, space_nombre: nuevoespacio.nombreEspacio})
+    //     if (error) {
+    //         console.error('Error inserting data:', error);
+    //     } else {
+    //         console.log('Data inserted successfully');
+    //     }
+    //
 }
 
 
--- src/supabaseconfig.js
+++ src/supabaseconfig.js
@@ -1,9 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
-// Create a single supabase client for interacting with your database
-const supabaseUrl = "https://fhayskinwatcghlakkmv.supabase.co" 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoYXlza2lud2F0Y2dobGFra212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE1ODUwNzYsImV4cCI6MjAzNzE2MTA3Nn0.cvdphUCwsjNhKCS6thX-V65TVs0iHwPP_zgX5iDBPvM"
+// // Create a single supabase client for interacting with your database
+// const supabaseUrl = "https://fhayskinwatcghlakkmv.supabase.co" 
+// const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoYXlza2lud2F0Y2dobGFra212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE1ODUwNzYsImV4cCI6MjAzNzE2MTA3Nn0.cvdphUCwsjNhKCS6thX-V65TVs0iHwPP_zgX5iDBPvM"
 
-const supabase = createClient(supabaseUrl,supabaseKey)
+// const supabase = createClient(supabaseUrl,supabaseKey)
 
-export default supabase
+// export default supabase
